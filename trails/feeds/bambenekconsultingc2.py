4073bdf161b4bcb81e2516e0cbeefc07191e035d
Miroslav Stampar <miroslav.stampar@gmail.com>
2015-03-14 17:14:50 +0100

    Minor fix

---
+++ trails/feeds/bambenekconsultingc2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+"""
+Copyright (c) 2014-2015 Miroslav Stampar (@stamparm)
+See the file 'LICENSE' for copying permission
+"""
+
+import re
+
+from core.common import retrieve_content
+from core.enums import TRAIL
+
+__type__ = (TRAIL.IP,)
+__url__ = "http://osint.bambenekconsulting.com/feeds/c2-ipmasterlist.txt"
+__check__ = "Master Feed"
+__reference__ = "bambenekconsulting.com"
+
+def fetch():
+    retval = dict((_, {}) for _ in __type__)
+    content = retrieve_content(__url__)
+
+    if __check__ in content:
+        for match in re.finditer(r"(?m)^([\d.]+),IP used by ([^,]+) C&C", content):
+            retval[TRAIL.IP][match.group(1)] = ("%s c&c" % match.group(2).lower(), __reference__)
+
+    return retval
