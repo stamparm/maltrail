3aa40400586d01554bd43eb96d10340795b92fe6
Miroslav Stampar <miroslav.stampar@gmail.com>
2015-03-15 06:09:13 +0100

    Minor update

--- trails/static/__init__.py
+++ trails/static/__init__.py
@@ -37,4 +37,24 @@ def fetch():
             else:
                 retval[TRAIL.DNS][line.strip('.')] = (__info__, __reference__)
 
+    for filename in glob.glob(os.path.join(os.path.dirname(__file__), "*.csv")):
+        __reference____ = "%s (static)" % os.path.splitext(os.path.basename(filename))[0]
+        content = open(filename, "rb").read()
+        for line in content.split('\n'):
+            line = line.strip()
+            if not line or line.startswith('#'):
+                continue
+            value, __info__ = line.split(',', 1)
+            __info__ = __info__.strip('"')
+            if '://' in value:
+                value = re.search(r"://(.*)", value).group(1)
+            value = value.rstrip('/')
+            if '/' in value:
+                retval[TRAIL.URL][value] = (__info__, __reference__)
+                value = value.split('/')[0]
+            elif re.search(r"\A\d+\.\d+\.\d+\.\d+\Z", value):
+                retval[TRAIL.IP][value] = (__info__, __reference__)
+            else:
+                retval[TRAIL.DNS][value.strip('.')] = (__info__, __reference__)
+
     return retval
