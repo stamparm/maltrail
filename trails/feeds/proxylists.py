c4ebf5f44549e2fac7558b0829dd6cc74b6c3f29
Miroslav Stampar <miroslav.stampar@gmail.com>
2015-10-28 11:57:26 +0100

    New feeds

---
+++ trails/feeds/proxylists.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+"""
+Copyright (c) 2014-2015 Miroslav Stampar (@stamparm)
+See the file 'LICENSE' for copying permission
+"""
+
+from core.common import retrieve_content
+
+__url__ = "https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/proxylists_30d.ipset"
+__check__ = "proxylists_30d"
+__info__ = "proxy (suspicious)"
+__reference__ = "proxylists.net"
+
+def fetch():
+    retval = {}
+    content = retrieve_content(__url__)
+
+    if __check__ in content:
+        for line in content.split('\n'):
+            line = line.strip()
+            if not line or line.startswith('#') or '.' not in line:
+                continue
+            retval[line.split('\t')[0]] = (__info__, __reference__)
+
+    return retval
---
+++ trails/feeds/sblam.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+"""
+Copyright (c) 2014-2015 Miroslav Stampar (@stamparm)
+See the file 'LICENSE' for copying permission
+"""
+
+from core.common import retrieve_content
+
+__url__ = "http://sblam.com/blacklist.txt"
+__check__ = "HTTP spam sources"
+__info__ = "HTTP spammer"
+__reference__ = "sblam.com"
+
+def fetch():
+    retval = {}
+    content = retrieve_content(__url__)
+
+    if __check__ in content:
+        for line in content.split('\n'):
+            line = line.strip()
+            if not line or line.startswith('#') or '.' not in line:
+                continue
+            retval[line.split('\t')[0]] = (__info__, __reference__)
+
+    return retval
