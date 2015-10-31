45fb915b7e208ca4b39c625219045da1b89f7787
Miroslav Stampar <miroslav.stampar@gmail.com>
2015-10-31 10:37:10 +0100

    New feed

---
+++ trails/feeds/rutgers.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+"""
+Copyright (c) 2014-2015 Miroslav Stampar (@stamparm)
+See the file 'LICENSE' for copying permission
+"""
+
+import re
+
+from core.common import retrieve_content
+
+__url__ = "http://report.rutgers.edu/DROP/attackers"
+__check__ = ".121."
+__info__ = "attacker"
+__reference__ = "rutgers.edu"
+
+def fetch():
+    retval = {}
+    content = retrieve_content(__url__)
+
+    if __check__ in content:
+        for line in content.split('\n'):
+            line = line.strip()
+            if not line or line.startswith('#') or '.' not in line:
+                continue
+            retval[line] = (__info__, __reference__)
+
+    return retval
