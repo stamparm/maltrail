906dd342b09f43227e949735a8b0d838bccd0a2b
Miroslav Stampar <miroslav.stampar@gmail.com>
2015-11-26 16:35:19 +0100

    Fixing problems with IE Compatibility View

--- core/settings.py
+++ core/settings.py
@@ -17,7 +17,7 @@ config = AttribDict()
 trails = {}
 
 NAME = "Maltrail"
-VERSION = "0.8.9"
+VERSION = "0.8.10"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
 DATE_FORMAT = "%Y-%m-%d"
 ROTATING_CHARS = ('\\', '|', '|', '/', '-')
