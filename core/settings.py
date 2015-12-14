18024c8b160a607031ef63264265257a7ea6b220
Miroslav Stampar <miroslav.stampar@gmail.com>
2015-12-14 09:48:06 +0100

    Minor dealing with false positives!

--- core/settings.py
+++ core/settings.py
@@ -17,7 +17,7 @@ config = AttribDict()
 trails = {}
 
 NAME = "Maltrail"
-VERSION = "0.8.137"
+VERSION = "0.8.138"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
 DATE_FORMAT = "%Y-%m-%d"
 ROTATING_CHARS = ('\\', '|', '|', '/', '-')
