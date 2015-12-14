a7641e7fff5615a8c5d2eda00139c0cbf0f1008d
Miroslav Stampar <miroslav.stampar@gmail.com>
2015-12-14 17:04:12 +0100

    Setting malware distribution to medium severity

--- core/settings.py
+++ core/settings.py
@@ -17,7 +17,7 @@ config = AttribDict()
 trails = {}
 
 NAME = "Maltrail"
-VERSION = "0.8.148"
+VERSION = "0.8.149"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
 DATE_FORMAT = "%Y-%m-%d"
 ROTATING_CHARS = ('\\', '|', '|', '/', '-')
