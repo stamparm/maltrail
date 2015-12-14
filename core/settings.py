623db5e5b6d7e3a3f44bfe82a452a255f308973e
Miroslav Stampar <miroslav.stampar@gmail.com>
2015-12-14 10:25:38 +0100

    Better info for malc0de feed

--- core/settings.py
+++ core/settings.py
@@ -17,7 +17,7 @@ config = AttribDict()
 trails = {}
 
 NAME = "Maltrail"
-VERSION = "0.8.139"
+VERSION = "0.8.140"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
 DATE_FORMAT = "%Y-%m-%d"
 ROTATING_CHARS = ('\\', '|', '|', '/', '-')
