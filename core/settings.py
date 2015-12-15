5ff4951b8d0c27433edadf9d261febe44ade7d52
Miroslav Stampar <miroslav.stampar@gmail.com>
2015-12-15 23:52:56 +0100

    Adding README.txt to the html directory

--- core/settings.py
+++ core/settings.py
@@ -17,7 +17,7 @@ config = AttribDict()
 trails = {}
 
 NAME = "Maltrail"
-VERSION = "0.8.169"
+VERSION = "0.8.171"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
 DATE_FORMAT = "%Y-%m-%d"
 ROTATING_CHARS = ('\\', '|', '|', '/', '-')
---
+++ html/README.txt
@@ -0,0 +1,1 @@
+Note: This directory should not be copied to the stand-alone web server installation (e.g. Apache) as it is being served by the server.py instance
