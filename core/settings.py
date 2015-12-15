31b27f03932b87f26c25f271ec17c4bac0372a34
Miroslav Stampar <miroslav.stampar@gmail.com>
2015-12-15 23:35:35 +0100

    Minor update (SSL support)

--- core/settings.py
+++ core/settings.py
@@ -17,7 +17,7 @@ config = AttribDict()
 trails = {}
 
 NAME = "Maltrail"
-VERSION = "0.8.166"
+VERSION = "0.8.167"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
 DATE_FORMAT = "%Y-%m-%d"
 ROTATING_CHARS = ('\\', '|', '|', '/', '-')
