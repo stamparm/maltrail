69bd6330f5ac8f963db26293bb0dd4a987e4dd41
Miroslav Stampar <miroslav.stampar@gmail.com>
2015-12-18 03:16:19 +0100

    Login bug fix

--- core/settings.py
+++ core/settings.py
@@ -17,7 +17,7 @@ config = AttribDict()
 trails = {}
 
 NAME = "Maltrail"
-VERSION = "0.8.210"
+VERSION = "0.8.211"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
 DATE_FORMAT = "%Y-%m-%d"
 ROTATING_CHARS = ('\\', '|', '|', '/', '-')
