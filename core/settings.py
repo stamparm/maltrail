0fbc728f9932e774f093c4a90e985303fa5ff58b
Miroslav Stampar <miroslav.stampar@gmail.com>
2015-12-29 21:57:02 +0100

    Preparation for login scheme change

--- core/settings.py
+++ core/settings.py
@@ -20,7 +20,7 @@ config = AttribDict()
 trails = {}
 
 NAME = "Maltrail"
-VERSION = "0.8.334"
+VERSION = "0.8.335"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
 DATE_FORMAT = "%Y-%m-%d"
 ROTATING_CHARS = ('\\', '|', '|', '/', '-')
