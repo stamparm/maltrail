17f961a8f8737bd1a7d4654d4fbe6a753b31599d
Miroslav Stampar <miroslav.stampar@gmail.com>
2016-01-03 01:07:35 +0100

    Update related to the generic sinkhole detection and removal of one obsolete feed

--- sensor.py
+++ sensor.py
@@ -188,7 +188,7 @@ def _process_ip(ip_data, sec, usec):
                 h_size = iph_length + (tcph_length << 2)
                 tcp_data = ip_data[h_size:]
 
-                if src_port == 80 and tcp_data.startswith("HTTP/") and "X-Sinkhole: Malware" in tcp_data[:tcp_data.find("\r\n\r\n")]:
+                if src_port == 80 and tcp_data.startswith("HTTP/") and any(_ in tcp_data[:tcp_data.find("\r\n\r\n")] for _ in ("X-Sinkhole:", "Server: Apache 1.0/SinkSoft")):
                     log_event((sec, usec, src_ip, src_port, dst_ip, dst_port, PROTO.TCP, TRAIL.IP, src_ip, "sinkhole response (malware)", "(heuristic)"))
 
                 method, path = None, None
