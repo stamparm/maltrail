17ad987ca60d2f013ca5a13954cd253071f9315f
Miroslav Stampar <miroslav.stampar@gmail.com>
2016-01-16 23:13:04 +0100

    Update of plugins

--- plugins/strings.py
+++ plugins/strings.py
@@ -17,6 +17,6 @@ def plugin(event_tuple, packet=None):
     if packet:
         localtime = time.strftime(TIME_FORMAT, time.localtime(int(sec)))
         output = "\n[%s] %s:%s -> %s:%s:\n" % (localtime, src_ip, src_port, dst_ip, dst_port)
-        output += "%s\n" % "".join(_ if _ in string.printable else '.' for _ in packet)
+        output += "%s\n" % "\n".join(re.findall(r"[ -~]{4,}", packet))
         sys.stderr.write(output)
         sys.stderr.flush()
