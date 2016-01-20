cb3dfcb455ac7029c170f36914f5c7561be13a06
Miroslav Stampar <miroslav.stampar@gmail.com>
2016-01-20 10:32:05 +0100

    Couple of UA fixes (reported by mail)

--- core/settings.py
+++ core/settings.py
@@ -21,7 +21,7 @@ config = AttribDict()
 trails = TrailsDict()
 
 NAME = "Maltrail"
-VERSION = "0.9.83"
+VERSION = "0.9.84"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
 DATE_FORMAT = "%Y-%m-%d"
 ROTATING_CHARS = ('\\', '|', '|', '/', '-')
