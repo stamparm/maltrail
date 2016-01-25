690bd99ba3f22a737fe2dd2d0abe4ee2319def26
Miroslav Stampar <miroslav.stampar@gmail.com>
2016-01-25 21:38:01 +0100

    Minor update

--- sensor.py
+++ sensor.py
@@ -259,7 +259,7 @@ def _process_packet(packet, sec, usec, ip_offset):
 
                 if config.USE_DEEP_HEURISTICS:
                     if tcp_data.startswith("HTTP/"):
-                        if any(_ in tcp_data[:tcp_data.find("\r\n\r\n")] for _ in ("X-Sinkhole:", "X-Malware-Sinkhole:", "Server: Apache 1.0/SinkSoft")) or "\r\n\r\nsinkhole" in tcp_data:
+                        if any(_ in tcp_data[:tcp_data.find("\r\n\r\n")] for _ in ("X-Sinkhole:", "X-Malware-Sinkhole:", "Server: You got served", "Server: Apache 1.0/SinkSoft", "sinkdns.org")) or "\r\n\r\nsinkhole" in tcp_data:
                             log_event((sec, usec, src_ip, src_port, dst_ip, dst_port, PROTO.TCP, TRAIL.IP, src_ip, "sinkhole response (malware)", "(heuristic)"), packet)
                         else:
                             index = tcp_data.find("<title>")
