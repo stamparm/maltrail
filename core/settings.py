e382c13c68ca10ae26a7ab853d79461721949a8a
Miroslav Stampar <miroslav.stampar@gmail.com>
2016-01-26 10:20:06 +0100

    Bug fix (proper condensing of DGA threats)

--- core/settings.py
+++ core/settings.py
@@ -22,7 +22,7 @@ config = AttribDict()
 trails = TrailsDict()
 
 NAME = "Maltrail"
-VERSION = "0.9.126"
+VERSION = "0.9.127"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
 DATE_FORMAT = "%Y-%m-%d"
 ROTATING_CHARS = ('\\', '|', '|', '/', '-')
