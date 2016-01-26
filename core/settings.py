stamparm/maltrail

commit 94048d77945cd64770a7fb5fca918aa9553dfc75

Minor optimization

core/settings.py
@@ -22,7 +22,7 @@ config = AttribDict()
 trails = TrailsDict()
 
 NAME = "Maltrail"
-VERSION = "0.9.115"
+VERSION = "0.9.116"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
 DATE_FORMAT = "%Y-%m-%d"
 ROTATING_CHARS = ('\\', '|', '|', '/', '-')

html/js/main.js
@@ -480,26 +480,28 @@ function init(url, from, to) {
 
             for (var i = 0; i < results.data.length; i++) {
                 var data = results.data[i];
+                var trail = data[LOG_COLUMNS.TRAIL];
+                var type = data[LOG_COLUMNS.TYPE];
 
                 if (data.length < LOG_COLUMNS_SIZE)
                     continue;
 
-                if (data[LOG_COLUMNS.TYPE] in COMMA_ENCODE_TRAIL_TYPES)
-                    data[LOG_COLUMNS.TRAIL] = data[LOG_COLUMNS.TRAIL].replace(/\,/g, "&#44;");
+                if (type in COMMA_ENCODE_TRAIL_TYPES)
+                    trail = trail.replace(/\,/g, "&#44;");
 
-                data[LOG_COLUMNS.TRAIL] = data[LOG_COLUMNS.TRAIL].replace(/\\\(/g, "&#40;").replace(/\\\)/g, "&#41;");
+                trail = trail.replace(/\\\(/g, "&#40;").replace(/\\\)/g, "&#41;");
 
-                var _ = data[LOG_COLUMNS.TRAIL].replace(/\([^)]+\)/g, "").replace(/\{[^}]+\}/g, "");
+                var _ = trail.replace(/\([^)]+\)/g, "");
+
+                if (!(type in TRAIL_TYPES))
+                    TRAIL_TYPES[type] = PREFERRED_TRAIL_COLORS[type] || getHashColor(type);
 
                 if (!(_ in trailSources))
-                    trailSources[_] = { };
-
-                if (!(data[LOG_COLUMNS.TYPE] in TRAIL_TYPES))
-                    TRAIL_TYPES[data[LOG_COLUMNS.TYPE]] = PREFERRED_TRAIL_COLORS[data[LOG_COLUMNS.TYPE]] || getHashColor(data[LOG_COLUMNS.TYPE]);
+                    trailSources[_] = {};
 
                 trailSources[_][data[LOG_COLUMNS.SRC_IP]] = true;
 
-                _ +=  " (" + data[LOG_COLUMNS.TYPE] + ")";
+                _ +=  " (" + type + ")";
                 if (!(_ in _TRAILS))
                     _TRAILS[_] = 1;
                 else

commit 7d07abcc9a388c1a07f2b85778a612df60a13580

Bug fix (introduced with 94048d77945cd64770a7fb5fca918aa9553dfc75#diff-20af0266d0281a9270b2139d42dfc5d9)

core/settings.py
@@ -22,7 +22,7 @@ config = AttribDict()
 trails = TrailsDict()
 
 NAME = "Maltrail"
-VERSION = "0.9.128"
+VERSION = "0.9.129"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
 DATE_FORMAT = "%Y-%m-%d"
 ROTATING_CHARS = ('\\', '|', '|', '/', '-')

html/js/main.js
@@ -493,7 +493,8 @@ function init(url, from, to) {
                 if (type in COMMA_ENCODE_TRAIL_TYPES)
                     trail = trail.replace(/\,/g, "&#44;");
 
-                var _ = trail.replace(/\\\(/g, "&#40;").replace(/\\\)/g, "&#41;").replace(/\([^)]+\)/g, "");
+                trail = data[LOG_COLUMNS.TRAIL] = trail.replace(/\\\(/g, "&#40;").replace(/\\\)/g, "&#41;")
+                var _ = trail.replace(/\([^)]+\)/g, "");
 
                 if (!(type in TRAIL_TYPES))
                     TRAIL_TYPES[type] = PREFERRED_TRAIL_COLORS[type] || getHashColor(type);