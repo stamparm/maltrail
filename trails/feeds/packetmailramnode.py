8aaa3daa1227264450a80a4da7d31754af160379
Miroslav Stampar <miroslav.stampar@gmail.com>
2016-01-30 23:46:40 +0100

    Adding new feeds

---
+++ trails/feeds/packetmailramnode.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+"""
+Copyright (c) 2014-2016 Miroslav Stampar (@stamparm)
+See the file 'LICENSE' for copying permission
+"""
+
+from core.common import retrieve_content
+
+__url__ = "https://www.packetmail.net/iprep_ramnode.txt"
+__check__ = ".1"
+__info__ = "known attacker"
+__reference__ = "packetmail.net"
+
+def fetch():
+    retval = {}
+    content = retrieve_content(__url__)
+
+    if __check__ in content:
+        for line in content.split('\n'):
+            line = line.strip()
+            if not line or line.startswith('#') or '.' not in line:
+                continue
+            retval[line.split(';')[0].strip()] = (__info__, __reference__)
+
+    return retval
---
+++ trails/feeds/urlvir.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+"""
+Copyright (c) 2014-2016 Miroslav Stampar (@stamparm)
+See the file 'LICENSE' for copying permission
+"""
+
+from core.common import retrieve_content
+
+__url__ = "http://www.urlvir.com/export-hosts/"
+__check__ = "Updated on"
+__info__ = "malware"
+__reference__ = "urlvir.com"
+
+def fetch():
+    retval = {}
+    content = retrieve_content(__url__)
+
+    if __check__ in content:
+        for line in content.split('\n'):
+            line = line.strip()
+            if not line or line.startswith('#') or '.' not in line:
+                continue
+            retval[line.strip()] = (__info__, __reference__)
+
+    return retval
