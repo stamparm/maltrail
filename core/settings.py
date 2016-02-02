4df8ca70e859a442132a73456ca6ec5915976490
Miroslav Stampar <miroslav.stampar@gmail.com>
2016-02-02 22:07:19 +0100

    Minor update (ipinfo domains)

--- core/settings.py
+++ core/settings.py
@@ -22,7 +22,7 @@ config = AttribDict()
 trails = TrailsDict()
 
 NAME = "Maltrail"
-VERSION = "0.9.155"
+VERSION = "0.9.156"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
 DATE_FORMAT = "%Y-%m-%d"
 ROTATING_CHARS = ('\\', '|', '|', '/', '-')
