a182392ef8fbb854fd291502d1744459b66505c1
Miroslav Stampar <miroslav.stampar@gmail.com>
2016-02-10 09:16:57 +0100

    Update of browser hijacking domains

--- core/settings.py
+++ core/settings.py
@@ -22,7 +22,7 @@ config = AttribDict()
 trails = TrailsDict()
 
 NAME = "Maltrail"
-VERSION = "0.9.198"
+VERSION = "0.9.199"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
 DATE_FORMAT = "%Y-%m-%d"
 ROTATING_CHARS = ('\\', '|', '|', '/', '-')
