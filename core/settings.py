be15e273f93abe34afb3930bd12c3b026b9ce9a8
Miroslav Stampar <miroslav.stampar@gmail.com>
2016-02-10 14:29:10 +0100

    Lets try to find old windows machines

--- core/settings.py
+++ core/settings.py
@@ -22,7 +22,7 @@ config = AttribDict()
 trails = TrailsDict()
 
 NAME = "Maltrail"
-VERSION = "0.9.204"
+VERSION = "0.9.205"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
 DATE_FORMAT = "%Y-%m-%d"
 ROTATING_CHARS = ('\\', '|', '|', '/', '-')
