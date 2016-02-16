f5137c6773dc6ca2a0b073949aebbda7e7a201c1
Miroslav Stampar <miroslav.stampar@gmail.com>
2016-02-16 10:45:26 +0100

    Removing leftover

--- core/settings.py
+++ core/settings.py
@@ -23,7 +23,7 @@ config = AttribDict()
 trails = TrailsDict()
 
 NAME = "Maltrail"
-VERSION = "0.9.221"
+VERSION = "0.9.222"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
 DATE_FORMAT = "%Y-%m-%d"
 ROTATING_CHARS = ('\\', '|', '|', '/', '-')
