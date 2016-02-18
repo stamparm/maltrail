04d848840bd865b51746c2fd4ab74f8aa8572f4a
Miroslav Stampar <miroslav.stampar@gmail.com>
2016-02-18 11:28:06 +0100

    New malware trails

--- core/settings.py
+++ core/settings.py
@@ -23,7 +23,7 @@ config = AttribDict()
 trails = TrailsDict()
 
 NAME = "Maltrail"
-VERSION = "0.9.245"
+VERSION = "0.9.247"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
 DATE_FORMAT = "%Y-%m-%d"
 ROTATING_CHARS = ('\\', '|', '|', '/', '-')
