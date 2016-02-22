d6e98cd1bdd529704576c5a4ce20ded6b7892bab
Miroslav Stampar <miroslav.stampar@gmail.com>
2016-02-22 09:52:51 +0100

    Minor commit

--- trails/feeds/malc0de.py
+++ trails/feeds/malc0de.py
@@ -9,7 +9,7 @@ import re
 
 from core.common import retrieve_content
 
-__url__ = "http://malc0de.com/bl/ZONES"
+__url__ = "https://malc0de.com/bl/ZONES"
 __check__ = "malc0de"
 __info__ = "malware distribution"
 __reference__ = "malc0de.com"
@@ -19,7 +19,7 @@ def fetch():
     content = retrieve_content(__url__)
 
     if __check__ in content:
-        for match in re.finditer(r'(?i)zone\s+"([^"]+)"\s+{"', content):
+        for match in re.finditer(r'(?i)zone\s+"([^"]+)"\s+{', content):
             retval[match.group(1)] = (__info__, __reference__)
 
     return retval
