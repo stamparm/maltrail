0897f86f34d7170dc38e9767b48c6bf12c9074f5
Miroslav Stampar <miroslav.stampar@gmail.com>
2016-03-10 17:04:01 +0100

    Minor revert

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ config = AttribDict()
 trails = TrailsDict()
 
 NAME = "Maltrail"
-VERSION = "0.10.77"
+VERSION = "0.10.78"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
 DATE_FORMAT = "%Y-%m-%d"
 ROTATING_CHARS = ('\\', '|', '|', '/', '-')
