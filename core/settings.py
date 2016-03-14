f57cb272da6aa47dca770e68444ff4629984591a
Miroslav Stampar <miroslav.stampar@gmail.com>
2016-03-14 09:12:56 +0100

    Adding new sinkhole and some malware trails

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ config = AttribDict()
 trails = TrailsDict()
 
 NAME = "Maltrail"
-VERSION = "0.10.91"
+VERSION = "0.10.92"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
 DATE_FORMAT = "%Y-%m-%d"
 ROTATING_CHARS = ('\\', '|', '|', '/', '-')
