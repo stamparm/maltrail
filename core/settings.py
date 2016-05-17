016430ecc4a3e768bda25e743409b655241877e7
Miroslav Stampar <miroslav.stampar@gmail.com>
2016-05-17 11:11:08 +0200

    Minor update of whitelist

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ config = AttribDict()
 trails = TrailsDict()
 
 NAME = "Maltrail"
-VERSION = "0.10.116"
+VERSION = "0.10.117"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
 DATE_FORMAT = "%Y-%m-%d"
 ROTATING_CHARS = ('\\', '|', '|', '/', '-')
