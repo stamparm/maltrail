959e1cf9373c43dfdffe3affce67456d7744b5ea
Binoj David <dbinoj@users.noreply.github.com>
2016-11-02 17:38:53 +0530

    Update maxmind trail
    Updated URL and regex.
    Maxmind started redirecting /proxy-detection-sample-list to /high-risk-ip-sample-list.

--- trails/feeds/maxmind.py
+++ trails/feeds/maxmind.py
@@ -9,9 +9,9 @@ import re
 
 from core.common import retrieve_content
 
-__url__ = "https://www.maxmind.com/en/proxy-detection-sample-list"
+__url__ = "https://www.maxmind.com/en/high-risk-ip-sample-list"
 __check__ = "Sample List of Higher Risk IP Addresses"
-__info__ = "anonymous proxy (suspicious)"
+__info__ = "high risk ip (suspicious)"
 __reference__ = "maxmind.com"
 
 def fetch():
@@ -19,7 +19,7 @@ def fetch():
     content = retrieve_content(__url__)
 
     if __check__ in content:
-        for match in re.finditer(r"proxy-detection-sample/([\d.]+)", content):
+        for match in re.finditer(r"high-risk-ip-sample/([\d.]+)", content):
             retval[match.group(1)] = (__info__, __reference__)
 
     return retval
