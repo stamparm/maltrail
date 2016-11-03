26febbe1695c486585a19caf02c40272cf5ffa8d
Yunus YILDIRIM <yunus__yildirim@hotmail.com>
2016-11-03 16:16:25 +0300

    some changes

--- trails/feeds/maxmind.py
+++ trails/feeds/maxmind.py
@@ -1,17 +1,12 @@
 #!/usr/bin/env python
 
-"""
-Copyright (c) 2014-2016 Miroslav Stampar (@stamparm)
-See the file 'LICENSE' for copying permission
-"""
-
 import re
 
 from core.common import retrieve_content
 
-__url__ = "https://www.maxmind.com/en/proxy-detection-sample-list"
+__url__ = "https://www.maxmind.com/en/high-risk-ip-sample-list"
 __check__ = "Sample List of Higher Risk IP Addresses"
-__info__ = "anonymous proxy (suspicious)"
+__info__ = "high risk ip (suspicious)"
 __reference__ = "maxmind.com"
 
 def fetch():
@@ -19,7 +14,7 @@ def fetch():
     content = retrieve_content(__url__)
 
     if __check__ in content:
-        for match in re.finditer(r"proxy-detection-sample/([\d.]+)", content):
+        for match in re.finditer(r"high-risk-ip-sample/([\d.]+)", content):
             retval[match.group(1)] = (__info__, __reference__)
 
     return retval
