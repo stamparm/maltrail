f75532fffc71ef6c703de49d3efedca1ac7e5f8b
John Kristoff <jtk@depaul.edu>
2017-01-05 23:17:25 -0600

    Added DataPlane.org feeds

---
+++ trails/feeds/dataplanesipquery.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+"""
+See the file 'LICENSE' for copying permission
+"""
+
+from core.common import retrieve_content
+
+__url__ = "https://dataplane.org/sipquery.txt"
+__check__ = "DataPlane.org"
+__info__ = "known SIP OPTIONS query"
+__reference__ = "dataplane.org"
+
+def fetch():
+    retval = {}
+    content = retrieve_content(__url__)
+
+    if __check__ in content:
+        for line in content.split('\n'):
+            line = line.strip()
+            if not line or line.startswith('#') or '.' not in line or '|' not in line:
+                continue
+            retval[line.split('|')[2].strip()] = (__info__, __reference__)
+
+    return retval
