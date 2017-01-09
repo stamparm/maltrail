9294ddcb71c42515afb540cbca3a26a0f3376892
Miroslav Stampar <miroslav.stampar@gmail.com>
2017-01-09 19:38:08 +0100

    Update for an Issue #119

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ config = AttribDict()
 trails = TrailsDict()
 
 NAME = "Maltrail"
-VERSION = "0.10.148"
+VERSION = "0.10.149"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
 DATE_FORMAT = "%Y-%m-%d"
 ROTATING_CHARS = ('\\', '|', '|', '/', '-')
