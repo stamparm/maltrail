6ed551d085e987be9ce917f6c68efc909bfb64c2
Miroslav Stampar <miroslav.stampar@gmail.com>
2017-02-09 22:37:40 +0100

    Proper commit related to the #8713f57324014045fedac4ca5fd4c741dfd32693

---
+++ trails/feeds/talosintelligence.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+"""
+Copyright (c) 2014-2016 Miroslav Stampar (@stamparm)
+See the file 'LICENSE' for copying permission
+"""
+
+from core.common import retrieve_content
+
+__url__ = "http://www.talosintelligence.com/feeds/ip-filter.blf"
+__check__ = ".1"
+__info__ = "bad reputation"
+__reference__ = "talosintelligence.com"
+
+# Reference: http://blog.snort.org/2015/09/ip-blacklist-feed-has-moved-locations.html
+def fetch():
+    retval = {}
+    content = retrieve_content(__url__)
+
+    if __check__ in content:
+        for line in content.split('\n'):
+            line = line.strip()
+            if not line or line.startswith('#') or '.' not in line:
+                continue
+            retval[line] = (__info__, __reference__)
+
+    return retval
