fb8b66c4d51795be5c704a9a10b7268c1400ec59
Miroslav Stampar <miroslav.stampar@gmail.com>
2017-02-10 12:38:50 +0100

    Update of Renocide trails

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ config = AttribDict()
 trails = TrailsDict()
 
 NAME = "Maltrail"
-VERSION = "0.10.170"
+VERSION = "0.10.171"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
 DATE_FORMAT = "%Y-%m-%d"
 ROTATING_CHARS = ('\\', '|', '|', '/', '-')
