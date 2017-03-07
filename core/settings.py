a149aa2225a267d1360893cd9d3cb8d6c10036d1
Miroslav Stampar <miroslav.stampar@gmail.com>
2017-03-07 10:43:46 +0100

    Adding DNSMessenger trails

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ config = AttribDict()
 trails = TrailsDict()
 
 NAME = "Maltrail"
-VERSION = "0.10.207"
+VERSION = "0.10.208"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
 DATE_FORMAT = "%Y-%m-%d"
 ROTATING_CHARS = ('\\', '|', '|', '/', '-')
