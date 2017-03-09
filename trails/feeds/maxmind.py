8b80a615d56b2c8d904b748f5e3e8156707f8556
Miroslav Stampar <miroslav.stampar@gmail.com>
2017-03-09 10:44:46 +0100

    Minor update

--- trails/feeds/maxmind.py
+++ trails/feeds/maxmind.py
@@ -11,7 +11,7 @@ from core.common import retrieve_content
 
 __url__ = "https://www.maxmind.com/en/high-risk-ip-sample-list"
 __check__ = "Sample List of Higher Risk IP Addresses"
-__info__ = "high risk ip (suspicious)"
+__info__ = "bad reputation (suspicious)"
 __reference__ = "maxmind.com"
 
 def fetch():
