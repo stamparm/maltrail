d22e1daaeffd17f41eee8c867bd47d5a949c1b7c
Miroslav Stampar <miroslav.stampar@gmail.com>
2017-05-03 15:53:08 +0200

    OpenBL is closing down

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ config = AttribDict()
 trails = TrailsDict()
 
 NAME = "Maltrail"
-VERSION = "0.10.259"
+VERSION = "0.10.260"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
 DATE_FORMAT = "%Y-%m-%d"
 ROTATING_CHARS = ('\\', '|', '|', '/', '-')
