36a22c948cd5a932b8867e0ad7cf62512c898280
Miroslav Stampar <miroslav.stampar@gmail.com>
2017-10-31 12:48:25 +0100

    Update of ransomware C&C domains

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ config = AttribDict()
 trails = TrailsDict()
 
 NAME = "Maltrail"
-VERSION = "0.10.290"
+VERSION = "0.10.291"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
 DATE_FORMAT = "%Y-%m-%d"
 ROTATING_CHARS = ('\\', '|', '|', '/', '-')
