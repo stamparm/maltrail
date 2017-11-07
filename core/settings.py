6a324557478549851d1da690c674235f702bd4de
Miroslav Stampar <miroslav.stampar@gmail.com>
2017-11-07 12:00:21 +0100

    Dirty patch

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ config = AttribDict()
 trails = TrailsDict()
 
 NAME = "Maltrail"
-VERSION = "0.10.296"
+VERSION = "0.10.297"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
 DATE_FORMAT = "%Y-%m-%d"
 ROTATING_CHARS = ('\\', '|', '|', '/', '-')
