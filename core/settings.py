30b89530ac80f2279c4ec1cfb67549776fb7a1ab
Miroslav Stampar <miroslav.stampar@gmail.com>
2017-12-07 12:43:10 +0100

    Minor update of Locky C&C domains

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ config = AttribDict()
 trails = TrailsDict()
 
 NAME = "Maltrail"
-VERSION = "0.10.303"
+VERSION = "0.10.304"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
 DATE_FORMAT = "%Y-%m-%d"
 ROTATING_CHARS = ('\\', '|', '|', '/', '-')
