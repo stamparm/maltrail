9b9242fba923a8f3835307f4268ac96810c9727d
Miroslav Stampar <miroslav.stampar@gmail.com>
2018-05-10 10:51:05 +0200

    New trail (rajump malware)

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ config = AttribDict()
 trails = TrailsDict()
 
 NAME = "Maltrail"
-VERSION = "0.10.331"
+VERSION = "0.10.332"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
 DATE_FORMAT = "%Y-%m-%d"
 ROTATING_CHARS = ('\\', '|', '|', '/', '-')
