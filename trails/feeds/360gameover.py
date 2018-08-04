27737bdd657631f733f69c789a45e4af057164e0
Miroslav Stampar <miroslav.stampar@gmail.com>
2018-08-04 16:31:17 +0200

    Update regarding #211

---
+++ trails/feeds/360gameover.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+"""
+Copyright (c) 2014-2018 Miroslav Stampar (@stamparm)
+See the file 'LICENSE' for copying permission
+"""
+
+import re
+
+from core.common import retrieve_content
+
+__url__ = "http://data.netlab.360.com/feeds/dga/gameover.txt"
+__check__ = "netlab 360"
+__info__ = "gameover dga (malware)"
+__reference__ = "360.com"
+
+def fetch():
+    retval = {}
+    content = retrieve_content(__url__)
+
+    if __check__ in content:
+        for match in re.finditer(r"(?m)^([\w.]+)\s+2\d{3}\-", content):
+            retval[match.group(1)] = (__info__, __reference__)
+
+    return retval
