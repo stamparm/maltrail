653976286ca00cf656cbf39d15f8f7f51d3ba141
Miroslav Stampar <miroslav.stampar@gmail.com>
2018-08-22 17:39:27 +0200

    Implementation of #241

---
+++ trails/feeds/urlhaus.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+"""
+Copyright (c) 2014-2018 Miroslav Stampar (@stamparm)
+See the file 'LICENSE' for copying permission
+"""
+
+import re
+
+from core.common import retrieve_content
+
+__url__ = "https://urlhaus.abuse.ch/downloads/text/"
+__check__ = "URLhaus"
+__info__ = "malware"
+__reference__ = "abuse.ch"
+
+def fetch():
+    retval = {}
+    content = retrieve_content(__url__)
+
+    if __check__ in content:
+        for line in content.split('\n'):
+            line = line.strip()
+            if not line or line.startswith('#'):
+                continue
+            if line.startswith("http") and "://" in line:
+                line = re.search(r"://(.*)", line).group(1)
+                retval[line] = (__info__, __reference__)
+
+    return retval
