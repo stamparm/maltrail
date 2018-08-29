065747f0edb44aa2361c0f40292e2ed2c6d85826
Miroslav Stampar <miroslav.stampar@gmail.com>
2018-08-29 15:43:32 +0200

    Moving Checkua to pup

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ config = AttribDict()
 trails = TrailsDict()
 
 NAME = "Maltrail"
-VERSION = "0.10.366"
+VERSION = "0.10.367"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
 DATE_FORMAT = "%Y-%m-%d"
 ROTATING_CHARS = ('\\', '|', '|', '/', '-')
