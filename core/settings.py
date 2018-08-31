14807ab3db010080aa80d154a4817f1cc80ae064
Miroslav Stampar <miroslav.stampar@gmail.com>
2018-08-31 12:35:34 +0200

    Adding feed for trickbot IPs (related to Issue #270)

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ config = AttribDict()
 trails = TrailsDict()
 
 NAME = "Maltrail"
-VERSION = "0.10.380"
+VERSION = "0.10.382"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
 DATE_FORMAT = "%Y-%m-%d"
 ROTATING_CHARS = ('\\', '|', '|', '/', '-')
---
+++ trails/feeds/trickbot.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+"""
+Copyright (c) 2014-2018 Miroslav Stampar (@stamparm)
+See the file 'LICENSE' for copying permission
+"""
+
+import re
+
+from core.addr import addr_to_int
+from core.addr import int_to_addr
+from core.addr import make_mask
+from core.common import retrieve_content
+
+__url__ = "https://github.com/JR0driguezB/malware_configs"
+__check__ = "mcconf"
+__info__ = "trickbot (malware)"
+__reference__ = "github.com/JR0driguezB"
+
+def fetch():
+    retval = {}
+    content = retrieve_content("https://github.com/JR0driguezB/malware_configs/tree/master/TrickBot/mcconf_files")
+
+    if __check__ in content:
+        last = re.findall(r"config.conf_\d+.xml", content)[-1]
+        content = retrieve_content("https://raw.githubusercontent.com/JR0driguezB/malware_configs/master/TrickBot/mcconf_files/%s" % last)
+        if __check__ in content:
+            for match in re.finditer(r"<srv>([\d.]+)", content):
+                retval[match.group(1)] = (__info__, __reference__)
+
+    return retval
