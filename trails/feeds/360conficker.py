12985d496f86545732f1eaa29558e94b6623f8c3
Miroslav Stampar <miroslav.stampar@gmail.com>
2018-09-04 10:42:23 +0200

    Switching 360 feeds to https because of retrieval speed

--- trails/feeds/360conficker.py
+++ trails/feeds/360conficker.py
@@ -9,7 +9,7 @@ import re
 
 from core.common import retrieve_content
 
-__url__ = "http://data.netlab.360.com/feeds/dga/conficker.txt"
+__url__ = "https://data.netlab.360.com/feeds/dga/conficker.txt"
 __check__ = "netlab 360"
 __info__ = "conficker dga (malware)"
 __reference__ = "360.com"
