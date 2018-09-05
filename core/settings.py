f16634d4bbe52041966bebca619f374137044447
Miroslav Stampar <miroslav.stampar@gmail.com>
2018-09-05 13:57:18 +0200

    Parking site

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ config = AttribDict()
 trails = TrailsDict()
 
 NAME = "Maltrail"
-VERSION = "0.10.398"
+VERSION = "0.10.399"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
 DATE_FORMAT = "%Y-%m-%d"
 ROTATING_CHARS = ('\\', '|', '|', '/', '-')
