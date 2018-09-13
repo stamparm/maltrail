aa4d98fcc7753716b1f12af3ad7f7dbe8a9af6f2
Miroslav Stampar <miroslav.stampar@gmail.com>
2018-09-13 13:23:15 +0200

    Minor update of sinkholes

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ config = AttribDict()
 trails = TrailsDict()
 
 NAME = "Maltrail"
-VERSION = "0.10.425"
+VERSION = "0.10.426"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
 DATE_FORMAT = "%Y-%m-%d"
 ROTATING_CHARS = ('\\', '|', '|', '/', '-')
