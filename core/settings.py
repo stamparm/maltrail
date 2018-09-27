bcd2172133fe8b77a9f8a47350d49ca374f28b2c
Miroslav Stampar <miroslav.stampar@gmail.com>
2018-09-27 10:41:37 +0200

    Merging jenxcus and dinihou

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ config = AttribDict()
 trails = TrailsDict()
 
 NAME = "Maltrail"
-VERSION = "0.10.448"
+VERSION = "0.10.449"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
 DATE_FORMAT = "%Y-%m-%d"
 ROTATING_CHARS = ('\\', '|', '|', '/', '-')
