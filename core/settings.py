8b15bef70cbea33b8b294c4a20a8e1d1305e271b
Miroslav Stampar <miroslav.stampar@gmail.com>
2018-09-27 13:00:09 +0200

    Update of NOIP sinkholes

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ config = AttribDict()
 trails = TrailsDict()
 
 NAME = "Maltrail"
-VERSION = "0.10.456"
+VERSION = "0.10.457"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
 DATE_FORMAT = "%Y-%m-%d"
 ROTATING_CHARS = ('\\', '|', '|', '/', '-')
