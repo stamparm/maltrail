be4bb5a98ad006c002ad0776700fbe4a5e8e5c99
Miroslav Stampar <miroslav.stampar@gmail.com>
2018-09-27 14:47:22 +0200

    Update of parking IPs

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ config = AttribDict()
 trails = TrailsDict()
 
 NAME = "Maltrail"
-VERSION = "0.10.461"
+VERSION = "0.10.462"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
 DATE_FORMAT = "%Y-%m-%d"
 ROTATING_CHARS = ('\\', '|', '|', '/', '-')
