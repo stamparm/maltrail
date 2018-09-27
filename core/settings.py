7ff27f51170fb3fe69570e01d7ab938e0639fde3
Miroslav Stampar <miroslav.stampar@gmail.com>
2018-09-27 21:18:20 +0200

    Minor update of sofacy domains

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ config = AttribDict()
 trails = TrailsDict()
 
 NAME = "Maltrail"
-VERSION = "0.10.465"
+VERSION = "0.10.466"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
 DATE_FORMAT = "%Y-%m-%d"
 ROTATING_CHARS = ('\\', '|', '|', '/', '-')
