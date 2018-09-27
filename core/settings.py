455719d989259a29bf5a9003e06a2a6140cbd97c
Miroslav Stampar <miroslav.stampar@gmail.com>
2018-09-27 10:34:52 +0200

    Merging apt_carbanak and apt_fin7

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ config = AttribDict()
 trails = TrailsDict()
 
 NAME = "Maltrail"
-VERSION = "0.10.445"
+VERSION = "0.10.446"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
 DATE_FORMAT = "%Y-%m-%d"
 ROTATING_CHARS = ('\\', '|', '|', '/', '-')
