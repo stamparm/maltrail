82a0bb39bfc377fff98e63c7a743b6aa97d1d833
Miroslav Stampar <miroslav.stampar@gmail.com>
2018-09-27 10:39:36 +0200

    Merging hworm and dinihou

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ config = AttribDict()
 trails = TrailsDict()
 
 NAME = "Maltrail"
-VERSION = "0.10.447"
+VERSION = "0.10.448"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
 DATE_FORMAT = "%Y-%m-%d"
 ROTATING_CHARS = ('\\', '|', '|', '/', '-')
