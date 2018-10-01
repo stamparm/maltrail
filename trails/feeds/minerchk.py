1b82cdfb1ee2137d39974242ac5fe5b1df2df4e2
Miroslav Stampar <miroslav.stampar@gmail.com>
2018-10-01 14:26:44 +0200

    Adding minerchk feed

---
+++ trails/feeds/minerchk.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+"""
+Copyright (c) 2014-2018 Miroslav Stampar (@stamparm)
+See the file 'LICENSE' for copying permission
+"""
+
+from core.common import retrieve_content
+
+__url__ = "https://raw.githubusercontent.com/Hestat/minerchk/master/hostslist.txt"
+__check__ = ".com"
+__info__ = "crypto mining (suspicious)"
+__reference__ = "github.com/Hestat"
+
+def fetch():
+    retval = {}
+    content = retrieve_content(__url__)
+
+    if __check__ in content:
+        for line in content.split('\n'):
+            line = line.strip()
+            if not line or line.startswith('#') or '.' not in line:
+                continue
+            retval[line] = (__info__, __reference__)
+
+    return retval
