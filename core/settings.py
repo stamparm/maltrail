a9136ac222d0f19fe847a406412b17eb096b35ae
Miroslav Stampar <miroslav.stampar@gmail.com>
2018-10-26 14:43:53 +0200

    Minor patch of sensor

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ config = AttribDict()
 trails = TrailsDict()
 
 NAME = "Maltrail"
-VERSION = "0.10.499"
+VERSION = "0.10.500"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
 DATE_FORMAT = "%Y-%m-%d"
 ROTATING_CHARS = ('\\', '|', '|', '/', '-')
