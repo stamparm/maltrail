f12c624a571e298aa09a710f24438455166dc851
Miroslav Stampar <miroslav.stampar@gmail.com>
2018-10-29 12:30:24 +0100

    Filtering out domains that can be found in immortal.txt (too)

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ config = AttribDict()
 trails = TrailsDict()
 
 NAME = "Maltrail"
-VERSION = "0.10.500"
+VERSION = "0.10.501"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
 DATE_FORMAT = "%Y-%m-%d"
 ROTATING_CHARS = ('\\', '|', '|', '/', '-')
