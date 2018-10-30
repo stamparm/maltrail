75e5fb8311c056631f52e43a062f502d9a8cbed5
Miroslav Stampar <miroslav.stampar@gmail.com>
2018-10-30 13:33:43 +0100

    Minor update

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ config = AttribDict()
 trails = TrailsDict()
 
 NAME = "Maltrail"
-VERSION = "0.10.501"
+VERSION = "0.10.502"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
 DATE_FORMAT = "%Y-%m-%d"
 ROTATING_CHARS = ('\\', '|', '|', '/', '-')
--- trails/feeds/loki.py
+++ trails/feeds/loki.py
@@ -9,7 +9,7 @@ import re
 
 from core.common import retrieve_content
 
-__url__ = "https://raw.githubusercontent.com/Neo23x0/signature-base/39787aaefa6b70b0be6e7dcdc425b65a716170ca/iocs/otx-c2-iocs.txt"
+__url__ = "https://raw.githubusercontent.com/Neo23x0/signature-base/master/iocs/otx-c2-iocs.txt"
 __check__ = "zapto"
 __info__ = "malware"
 __reference__ = "otx.alienvault.com"
@@ -27,7 +27,7 @@ def fetch():
             if re.search(r"\d+\.\d+\.\d+\.\d+", items[0]):
                 continue
             info = __info__
-            for _ in ('aaeh', 'andromeda', 'angler', 'anunak', 'arid viper', 'armageddon', 'asprox', 'babar', 'bandachor', 'bedep', 'black vine', 'buhtrap', 'camerashy', 'carbanak', 'cleaver', 'cmstar', 'cryptofortress', 'ctb-locker', 'darkhotel', 'darpapox', 'deep panda', 'desert falcons', 'destover', 'dragonok', 'dridex', 'dyre', 'el machete', 'elastic botnet', 'elf.billgates', 'equationdrug', 'escelar', 'evilgrab', 'fessleak', 'filmkan', 'flame', 'gamapos', 'gauss', 'gaza cybergang', 'grabit', 'group-3390', 'hellsing', 'kazy', 'keyraider', 'kriptovor', 'lotus blossom', 'moose', 'neutrino', 'nitlovepos', 'nuclear', 'pkybot', 'plugx', 'plugx', 'poison ivy', 'pony', 'poseidon', 'potao express', 'pushdo', 'red october', 'regin', 'retefe', 'rig', 'rocket kitten', 'rsa ir', 'sakula', 'sandworm', 'shade encryptor', 'shell crew', 'signed pos', 'skype worm', 'sofacy', 'steamstealers', 'stuxnet', 'symmi', 'teslacrypt', 'the equation', 'the masked', 'the naikon', 'torrentlocker', 'trapwot', 'triplenine', 'turla', 'volatile cedar', 'windigo', 'wintti', 'wirelurker', 'word intruder', 'xlscmd', 'zeuscart'):
+            for _ in ('aaeh', 'andromeda', 'anunak', 'arid viper', 'armageddon', 'asprox', 'babar', 'bandachor', 'bedep', 'black vine', 'buhtrap', 'camerashy', 'carbanak', 'cleaver', 'cmstar', 'cryptofortress', 'ctb-locker', 'darkhotel', 'darpapox', 'deep panda', 'desert falcons', 'destover', 'dragonok', 'dridex', 'dyre', 'el machete', 'elastic botnet', 'elf.billgates', 'equationdrug', 'escelar', 'evilgrab', 'fessleak', 'filmkan', 'flame', 'gamapos', 'gauss', 'gaza cybergang', 'grabit', 'group-3390', 'hellsing', 'kazy', 'keyraider', 'kriptovor', 'lotus blossom', 'moose', 'neutrino', 'nitlovepos', 'nuclear', 'pkybot', 'plugx', 'plugx', 'poison ivy', 'pony', 'poseidon', 'potao express', 'pushdo', 'red october', 'regin', 'retefe', 'rig', 'rocket kitten', 'rsa ir', 'sakula', 'sandworm', 'shade encryptor', 'shell crew', 'signed pos', 'skype worm', 'sofacy', 'steamstealers', 'stuxnet', 'symmi', 'teslacrypt', 'the equation', 'the masked', 'the naikon', 'torrentlocker', 'trapwot', 'triplenine', 'turla', 'volatile cedar', 'windigo', 'wintti', 'wirelurker', 'word intruder', 'xlscmd', 'zeuscart'):
                 if re.search(r"(?i)\b%s\b" % _, items[1]):
                     info = "%s (malware)" % _
                     break
