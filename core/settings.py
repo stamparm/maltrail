0d4b0109bbb33f9926c163789879cdfd712fda57
Miroslav Stampar <miroslav.stampar@gmail.com>
2018-11-07 14:48:37 +0100

    Minor update of ipinfo providers

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ config = AttribDict()
 trails = TrailsDict()
 
 NAME = "Maltrail"
-VERSION = "0.11.1"
+VERSION = "0.11.2"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
 DATE_FORMAT = "%Y-%m-%d"
 ROTATING_CHARS = ('\\', '|', '|', '/', '-')
