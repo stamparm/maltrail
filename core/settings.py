d75176affd3b2cbd4010c0c18b5274f54ea0e870
Miroslav Stampar <miroslav.stampar@gmail.com>
2019-02-28 15:48:41 +0100

    Trivial update of late pull merges

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ config = AttribDict()
 trails = TrailsDict()
 
 NAME = "Maltrail"
-VERSION = "0.11.38"
+VERSION = "0.11.39"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
 DATE_FORMAT = "%Y-%m-%d"
 ROTATING_CHARS = ('\\', '|', '|', '/', '-')
