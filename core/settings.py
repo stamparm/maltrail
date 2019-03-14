05c18e2ee076fb4310c3a351bf07052d8d210c7d
Miroslav Stampar <miroslav.stampar@gmail.com>
2019-03-14 12:05:24 +0100

    Dealing with some old (traffic spotted) malware

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ config = AttribDict()
 trails = TrailsDict()
 
 NAME = "Maltrail"
-VERSION = "0.12.7"
+VERSION = "0.12.8"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
 DATE_FORMAT = "%Y-%m-%d"
 ROTATING_CHARS = ('\\', '|', '|', '/', '-')
