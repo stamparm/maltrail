8ec62f270ce2cff590bb7695d89086e2dda80c7f
Miroslav Stampar <miroslav.stampar@gmail.com>
2019-03-21 11:28:46 +0100

    Dear Travis CI, goodbye and thanks for all the fish

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ config = AttribDict()
 trails = TrailsDict()
 
 NAME = "Maltrail"
-VERSION = "0.12.13"
+VERSION = "0.12.14"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
 DATE_FORMAT = "%Y-%m-%d"
 ROTATING_CHARS = ('\\', '|', '|', '/', '-')
