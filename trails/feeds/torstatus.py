e4d1dd8c6f9a4e7289ac4eb9afebcfd642622008
deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-04-08 13:28:36 -0700

    Fixed PYTHON_E302 issues in trails/feeds/torstatus.py

--- trails/feeds/torstatus.py
+++ trails/feeds/torstatus.py
@@ -11,6 +11,7 @@ __url__ = "https://torstatus.blutmagie.de/ip_list_all.php/Tor_ip_list_ALL.csv"
 __info__ = "tor exit node (suspicious)"
 __reference__ = "blutmagie.de"
 
+
 def fetch():
     retval = {}
     content = retrieve_content(__url__)
