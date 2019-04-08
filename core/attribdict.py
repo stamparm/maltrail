f68bf29aa0c017f02f50c6103ba6380b23a6d3d4
deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-04-08 13:31:40 -0700

    Fixed PYTHON_E302 issues in core/attribdict.py

--- core/attribdict.py
+++ core/attribdict.py
@@ -5,6 +5,7 @@ Copyright (c) 2014-2019 Maltrail developers (https://github.com/stamparm/maltrai
 See the file 'LICENSE' for copying permission
 """
 
+
 class AttribDict(dict):
     def __getattr__(self, name):
         return self.get(name)
