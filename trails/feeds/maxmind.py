bfb7e1acbd6ab0d2573a89fa5232a5804da05ad4
deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-04-08 13:31:04 -0700

    Fixed PYTHON_E302 issues in trails/feeds/maxmind.py

--- trails/feeds/maxmind.py
+++ trails/feeds/maxmind.py
@@ -14,6 +14,7 @@ __check__ = "Sample List of Higher Risk IP Addresses"
 __info__ = "bad reputation (suspicious)"
 __reference__ = "maxmind.com"
 
+
 def fetch():
     retval = {}
     content = retrieve_content(__url__)
