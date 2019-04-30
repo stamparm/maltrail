0e01942ab7dcc6110ee499cbb414225a6a61dde1
Miroslav Stampar <miroslav.stampar@gmail.com>
2019-04-30 14:26:20 +0200

    Version bump

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ config = AttribDict()
 trails = TrailsDict()
 
 NAME = "Maltrail"
-VERSION = "0.12.33"
+VERSION = "0.13.2"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
 DATE_FORMAT = "%Y-%m-%d"
 ROTATING_CHARS = ('\\', '|', '|', '/', '-')
