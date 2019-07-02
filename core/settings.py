b360b96ee6120f78963baf89d1940db9ca469b4d
Miroslav Stampar <miroslav.stampar@gmail.com>
2019-07-02 13:13:25 +0200

    Update regarding #2423

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ config = AttribDict()
 trails = TrailsDict()
 
 NAME = "Maltrail"
-VERSION = "0.13.20"
+VERSION = "0.13.21"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
 DATE_FORMAT = "%Y-%m-%d"
 ROTATING_CHARS = ('\\', '|', '|', '/', '-')
--- html/js/errorhandler.js
+++ html/js/errorhandler.js
@@ -1,6 +1,16 @@
 window.onerror = function(errorMsg, url, lineNumber) {
-    if (typeof errorMsg !== "undefined")
-        alert(errorMsg + ' Script: ' + url + ' Line: ' + lineNumber);
+    if (typeof errorMsg !== "string") {
+        if (errorMsg.hasOwnProperty("filename"))
+            url = errorMsg.filename;
+
+        if (errorMsg.hasOwnProperty("lineno"))
+            lineNumber = errorMsg.lineno;
+
+        if (errorMsg.hasOwnProperty("message"))
+            errorMsg = errorMsg.message;
+    }
+
+    alert(errorMsg + ' Script: ' + url + ' Line: ' + lineNumber);
 
     try {
         $("body").loader("hide");
