379cf84caec2bdc6904bbf3a5ccdb9f8047a589d
Miroslav Stampar <miroslav.stampar@gmail.com>
2019-07-17 09:45:47 +0200

    All false positives (Amazon)

--- core/settings.py
+++ core/settings.py
@@ -21,7 +21,7 @@ from core.attribdict import AttribDict
 from core.trailsdict import TrailsDict
 
 NAME = "Maltrail"
-VERSION = "0.13.38"
+VERSION = "0.13.39"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
 DATE_FORMAT = "%Y-%m-%d"
 ROTATING_CHARS = ('\\', '|', '|', '/', '-')
