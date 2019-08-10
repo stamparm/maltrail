62a089ed8ba17816862936cbce013023d3f7353c
Miroslav Stampar <miroslav.stampar@gmail.com>
2019-08-11 00:41:56 +0200

    Proper update for #3215

--- core/settings.py
+++ core/settings.py
@@ -21,7 +21,7 @@ from core.attribdict import AttribDict
 from core.trailsdict import TrailsDict
 
 NAME = "Maltrail"
-VERSION = "0.13.49"
+VERSION = "0.13.50"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
 DATE_FORMAT = "%Y-%m-%d"
 ROTATING_CHARS = ('\\', '|', '|', '/', '-')
