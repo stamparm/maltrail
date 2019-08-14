37f7a4be0da57a5639549388db567948cd7cbba4
Miroslav Stampar <miroslav.stampar@gmail.com>
2019-08-14 13:15:38 +0200

    FP removal (Facebook's CDN)

--- core/settings.py
+++ core/settings.py
@@ -21,7 +21,7 @@ from core.attribdict import AttribDict
 from core.trailsdict import TrailsDict
 
 NAME = "Maltrail"
-VERSION = "0.13.51"
+VERSION = "0.13.52"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
 DATE_FORMAT = "%Y-%m-%d"
 ROTATING_CHARS = ('\\', '|', '|', '/', '-')
