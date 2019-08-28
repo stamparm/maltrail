986197f61198901b93c076612d371c182cd19aa1
Miroslav Stampar <miroslav.stampar@gmail.com>
2019-08-28 12:36:36 +0200

    Minor update of attrs

--- core/settings.py
+++ core/settings.py
@@ -21,7 +21,7 @@ from core.attribdict import AttribDict
 from core.trailsdict import TrailsDict
 
 NAME = "Maltrail"
-VERSION = "0.13.61"
+VERSION = "0.13.63"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
 DATE_FORMAT = "%Y-%m-%d"
 ROTATING_CHARS = ('\\', '|', '|', '/', '-')
