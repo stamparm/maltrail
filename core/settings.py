e0694a76771f40220e68ec79c118b2bc866fee28
Miroslav Stampar <miroslav.stampar@gmail.com>
2019-09-10 15:36:53 +0200

    Minor patch for #162

--- core/settings.py
+++ core/settings.py
@@ -22,7 +22,7 @@ from core.trailsdict import TrailsDict
 from thirdparty.six.moves import urllib as _urllib
 
 NAME = "Maltrail"
-VERSION = "0.13.83"
+VERSION = "0.13.84"
 PLATFORM = os.name
 PYVERSION = sys.version.split()[0]
 IS_WIN = PLATFORM == "nt"
