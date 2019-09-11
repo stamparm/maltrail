b93579d9c486eb513e895bd4d5292c478b39f095
Miroslav Stampar <miroslav.stampar@gmail.com>
2019-09-11 11:32:04 +0200

    Unleash the Dragon (#162)

--- core/versioncheck.py
+++ core/versioncheck.py
@@ -5,10 +5,9 @@ Copyright (c) 2014-2019 Maltrail developers (https://github.com/stamparm/maltrai
 See the file 'LICENSE' for copying permission
 """
 
-import os
 import sys
 
 PYVERSION = sys.version.split()[0]
 
-if (PYVERSION >= "3" or PYVERSION < "2.6") and not int(os.environ.get("MALTRAIL_DREI", 0)):
-    exit("[CRITICAL] incompatible Python version detected ('%s'). For successfully running Maltrail you'll have to use version 2.6 or 2.7 (visit 'http://www.python.org/download/')" % PYVERSION)
+if PYVERSION < "2.6":
+    sys.exit("[%s] [CRITICAL] incompatible Python version detected ('%s'). To successfully run Maltrail you'll have to use version 2.6, 2.7 or 3.x (visit 'https://www.python.org/downloads/')" % PYVERSION)
