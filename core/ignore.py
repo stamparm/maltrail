5ee019f75396e836db6840dac565d45873b6871a
Miroslav Stampar <miroslav.stampar@gmail.com>
2019-09-17 09:20:44 +0200

    Adding configuration option IGNORE_EVENTS_REGEX

--- core/ignore.py
+++ core/ignore.py
@@ -8,12 +8,17 @@ from __future__ import print_function
 
 # simple ignore rule mechanism configured by file 'misc/ignore_event.txt' and/or user defined `USER_IGNORELIST`
 
+import re
+
 from core.settings import config
 from core.settings import IGNORE_EVENTS
 
 def ignore_event(event_tuple):
     retval = False
     _, _, src_ip, src_port, dst_ip, dst_port, _, _, _, _, _ = event_tuple
+
+    if config.IGNORE_EVENTS_REGEX and re.search(config.IGNORE_EVENTS_REGEX, repr(event_tuple), re.I):
+        retval = True
 
     for ignore_src_ip, ignore_src_port, ignore_dst_ip, ignore_dst_port in IGNORE_EVENTS:
         if ignore_src_ip != '*' and ignore_src_ip != src_ip :
