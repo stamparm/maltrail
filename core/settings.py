703a2128d625ac884baac3c2e7755cd63dbd7f63
Miroslav Stampar <miroslav.stampar@gmail.com>
2019-09-19 14:54:46 +0200

    Actually fin9 (found by duplicates/overlapping)

--- core/settings.py
+++ core/settings.py
@@ -21,7 +21,7 @@ from core.trailsdict import TrailsDict
 from thirdparty.six.moves import urllib as _urllib
 
 NAME = "Maltrail"
-VERSION = "0.13.122"
+VERSION = "0.13.123"
 PLATFORM = os.name
 IS_WIN = PLATFORM == "nt"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
