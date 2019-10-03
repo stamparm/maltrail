8848ee7d3cbd97cba7e6faf5f9a831134de8bebf
Miroslav Stampar <miroslav.stampar@gmail.com>
2019-10-03 15:42:20 +0200

    Minor update

--- core/settings.py
+++ core/settings.py
@@ -21,7 +21,7 @@ from core.trailsdict import TrailsDict
 from thirdparty.six.moves import urllib as _urllib
 
 NAME = "Maltrail"
-VERSION = "0.15.8"
+VERSION = "0.15.9"
 PLATFORM = os.name
 IS_WIN = PLATFORM == "nt"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
