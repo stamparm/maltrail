5c417ea45a442d711088ab2ca8be0f7a0d25dae9
Miroslav Stampar <miroslav.stampar@gmail.com>
2019-10-07 11:47:21 +0200

    Some (satellite) domains were missing

--- core/settings.py
+++ core/settings.py
@@ -21,7 +21,7 @@ from core.trailsdict import TrailsDict
 from thirdparty.six.moves import urllib as _urllib
 
 NAME = "Maltrail"
-VERSION = "0.15.11"
+VERSION = "0.15.12"
 PLATFORM = os.name
 IS_WIN = PLATFORM == "nt"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
