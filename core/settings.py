81a790293f455a2cf219a022743f191504503ad7
Miroslav Stampar <miroslav.stampar@gmail.com>
2019-10-16 15:47:54 +0200

    Minor patch (ta505 FPs with dropbox-dns.com)

--- core/settings.py
+++ core/settings.py
@@ -21,7 +21,7 @@ from core.trailsdict import TrailsDict
 from thirdparty.six.moves import urllib as _urllib
 
 NAME = "Maltrail"
-VERSION = "0.15.31"
+VERSION = "0.15.32"
 PLATFORM = os.name
 IS_WIN = PLATFORM == "nt"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
