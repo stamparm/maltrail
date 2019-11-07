e58f45ab5c109e9fe797e059ba46844cf5866520
Miroslav Stampar <miroslav.stampar@gmail.com>
2019-11-07 11:36:30 +0100

    FPs (dummy ad JS)

--- core/settings.py
+++ core/settings.py
@@ -21,7 +21,7 @@ from core.trailsdict import TrailsDict
 from thirdparty.six.moves import urllib as _urllib
 
 NAME = "Maltrail"
-VERSION = "0.16.25"
+VERSION = "0.16.26"
 PLATFORM = os.name
 IS_WIN = PLATFORM == "nt"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
