0a58a95a447a597befe3a61d455cc98f04074906
Miroslav Stampar <miroslav.stampar@gmail.com>
2019-12-10 14:02:55 +0100

    Typo squatting domains (not malware)

--- core/settings.py
+++ core/settings.py
@@ -21,7 +21,7 @@ from core.trailsdict import TrailsDict
 from thirdparty.six.moves import urllib as _urllib
 
 NAME = "Maltrail"
-VERSION = "0.16.145"
+VERSION = "0.16.146"
 PLATFORM = os.name
 IS_WIN = PLATFORM == "nt"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
