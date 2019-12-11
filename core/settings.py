4ccb3216b7aa6192db926b665964b2adb1b60a36
Miroslav Stampar <miroslav.stampar@gmail.com>
2019-12-11 10:42:31 +0100

    Found as FP in regular traffic

--- core/settings.py
+++ core/settings.py
@@ -21,7 +21,7 @@ from core.trailsdict import TrailsDict
 from thirdparty.six.moves import urllib as _urllib
 
 NAME = "Maltrail"
-VERSION = "0.16.153"
+VERSION = "0.16.154"
 PLATFORM = os.name
 IS_WIN = PLATFORM == "nt"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
