e311775cdb1803190d64d70d0be54e83f475d74d
Miroslav Stampar <miroslav.stampar@gmail.com>
2019-12-13 10:02:30 +0100

    Regex compress (making noise in regular reports as all of them are contacted)

--- core/settings.py
+++ core/settings.py
@@ -21,7 +21,7 @@ from core.trailsdict import TrailsDict
 from thirdparty.six.moves import urllib as _urllib
 
 NAME = "Maltrail"
-VERSION = "0.16.162"
+VERSION = "0.16.163"
 PLATFORM = os.name
 IS_WIN = PLATFORM == "nt"
 SERVER_HEADER = "%s/%s" % (NAME, VERSION)
