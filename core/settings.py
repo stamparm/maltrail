bcf7493a2354485226d09e9f27bb14bdf103a858
Miroslav Stampar <miroslav.stampar@gmail.com>
2020-01-14 22:32:00 +0100

    Minor update of trails

--- core/settings.py
+++ core/settings.py
@@ -21,7 +21,7 @@ from core.trailsdict import TrailsDict
 from thirdparty.six.moves import urllib as _urllib
 
 NAME = "Maltrail"
-VERSION = "0.16.209"
+VERSION = "0.16.210"
 PLATFORM = os.name
 IS_WIN = PLATFORM == "nt"
 IS_SENSOR = sys.argv[0].startswith("sensor")
