94d3648854e82ddf19609f7751738d79cce78719
Miroslav Stampar <miroslav.stampar@gmail.com>
2020-02-27 10:12:47 +0100

    Minor update

--- core/settings.py
+++ core/settings.py
@@ -21,7 +21,7 @@ from core.trailsdict import TrailsDict
 from thirdparty.six.moves import urllib as _urllib
 
 NAME = "Maltrail"
-VERSION = "0.17.68"
+VERSION = "0.17.69"
 PLATFORM = os.name
 IS_WIN = PLATFORM == "nt"
 IS_SENSOR = sys.argv[0].startswith("sensor")
