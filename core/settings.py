26084f8c2e4ec688e706aec1b543f658b7abe979
Miroslav Stampar <miroslav.stampar@gmail.com>
2020-05-01 01:01:01 +0200

    Tag version bump

--- core/settings.py
+++ core/settings.py
@@ -21,7 +21,7 @@ from core.trailsdict import TrailsDict
 from thirdparty.six.moves import urllib as _urllib
 
 NAME = "Maltrail"
-VERSION = "0.19.55"
+VERSION = "0.20.1"
 PLATFORM = os.name
 IS_WIN = PLATFORM == "nt"
 IS_SENSOR = sys.argv[0].startswith("sensor")
