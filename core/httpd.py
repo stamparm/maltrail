38d9badb3d81bbc1ab5e6f03d5cf1cc9bd2a3bf6
Miroslav Stampar <miroslav.stampar@gmail.com>
2020-09-07 00:17:05 +0200

    Fixes #10874

--- core/httpd.py
+++ core/httpd.py
@@ -29,7 +29,6 @@ from core.common import get_regex
 from core.common import ipcat_lookup
 from core.common import worst_asns
 from core.compat import xrange
-from core.datatype import LRUDict
 from core.enums import HTTP_HEADER
 from core.settings import config
 from core.settings import CONTENT_EXTENSIONS_EXCLUSIONS
@@ -40,7 +39,6 @@ from core.settings import HTML_DIR
 from core.settings import HTTP_TIME_FORMAT
 from core.settings import IS_WIN
 from core.settings import MAX_NOFILE
-from core.settings import MAX_RESULT_CACHE_ENTRIES
 from core.settings import NAME
 from core.settings import PING_RESPONSE
 from core.settings import SESSION_COOKIE_NAME
@@ -70,7 +68,8 @@ try:
 except:
     pass
 
-_result_cache = LRUDict(MAX_RESULT_CACHE_ENTRIES)
+_fail2ban_cache = None
+_fail2ban_key = None
 
 def start_httpd(address=None, port=None, join=False, pem=None):
     """
@@ -443,12 +442,15 @@ def start_httpd(address=None, port=None, join=False, pem=None):
             return PING_RESPONSE
 
         def _fail2ban(self, params):
+            global _fail2ban_cache
+            global _fail2ban_key
+
             self.send_response(_http_client.OK)
             self.send_header(HTTP_HEADER.CONNECTION, "close")
             self.send_header(HTTP_HEADER.CONTENT_TYPE, "text/plain")
 
             content = ""
-            key = "fail2ban|%d" % (int(time.time()) >> 3)
+            key = int(time.time()) >> 3
 
             if config.FAIL2BAN_REGEX:
                 try:
@@ -456,8 +458,8 @@ def start_httpd(address=None, port=None, join=False, pem=None):
                 except re.error:
                     content = "invalid regular expression used in option FAIL2BAN_REGEX"
                 else:
-                    if key in _result_cache:
-                        content = _result_cache[key]
+                    if key == _fail2ban_key:
+                        content = _fail2ban_cache
                     else:
                         result = set()
                         _ = os.path.join(config.LOG_DIR, "%s.log" % datetime.datetime.now().strftime("%Y-%m-%d"))
@@ -467,7 +469,9 @@ def start_httpd(address=None, port=None, join=False, pem=None):
                                     result.add(line.split()[3])
 
                         content = "\n".join(result)
-                        _result_cache[key] = content
+
+                        _fail2ban_cache = content
+                        _fail2ban_key = key
             else:
                 content = "configuration option FAIL2BAN_REGEX not set"
 
