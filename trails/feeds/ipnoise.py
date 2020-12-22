8bf3df527a3f875ea8d9f9fb2384b05ec6f132b6
Miroslav Stampar <miroslav.stampar@gmail.com>
2020-12-22 10:47:05 +0100

    Adding new feed

---
+++ trails/feeds/ipnoise.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+"""
+Copyright (c) 2014-2020 Maltrail developers (https://github.com/stamparm/maltrail/)
+See the file 'LICENSE' for copying permission
+"""
+
+from core.common import retrieve_content
+
+__url__ = "http://ipnoise.undo.it/ssh_blacklist.txt"
+__check__ = ", ipnoise"
+__info__ = "known attacker"
+__reference__ = "ipnoise.undo.it"
+
+def fetch():
+    retval = {}
+    content = retrieve_content(__url__)
+
+    if __check__ in content:
+        for line in content.split('\n'):
+            line = line.strip()
+            if not line or line.startswith('#') or '.' not in line:
+                continue
+            retval[line] = (__info__, __reference__)
+
+    return retval
