1b7c1de0f3a8e2c02e4e2645ccfbdae7168ab30e
Miroslav Stampar <miroslav.stampar@gmail.com>
2021-01-14 14:00:20 +0100

    VT doesn't like undo.it

--- trails/feeds/ipnoise.py
+++ trails/feeds/ipnoise.py
@@ -7,10 +7,10 @@ See the file 'LICENSE' for copying permission
 
 from core.common import retrieve_content
 
-__url__ = "http://ipnoise.undo.it/blacklist.txt"
+__url__ = "http://ipnoise.now.im/blacklist.txt"
 __check__ = ", ipnoise"
 __info__ = "known attacker"
-__reference__ = "ipnoise.undo.it"
+__reference__ = "ipnoise.now.im"
 
 def fetch():
     retval = {}
