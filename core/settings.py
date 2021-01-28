31fd6252620512735ba758b84bcfbd84c74b8aa5
Miroslav Stampar <miroslav.stampar@gmail.com>
2021-01-28 21:31:43 +0100

    Minor update of trails

--- core/settings.py
+++ core/settings.py
@@ -23,7 +23,7 @@ from core.trailsdict import TrailsDict
 from thirdparty.six.moves import urllib as _urllib
 
 NAME = "Maltrail"
-VERSION = "0.28.92"
+VERSION = "0.28.93"
 HOMEPAGE = "https://maltrail.github.io"
 PLATFORM = os.name
 IS_WIN = PLATFORM == "nt"
