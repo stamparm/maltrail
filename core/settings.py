f6261a355138fb00bfc9906549dddcbcc5d9d83f
Miroslav Stampar <miroslav.stampar@gmail.com>
2021-05-16 20:37:16 +0200

    Restoring TPs

--- core/settings.py
+++ core/settings.py
@@ -23,7 +23,7 @@ from core.trailsdict import TrailsDict
 from thirdparty.six.moves import urllib as _urllib
 
 NAME = "Maltrail"
-VERSION = "0.32.5"
+VERSION = "0.32.6"
 HOMEPAGE = "https://maltrail.github.io"
 PLATFORM = os.name
 IS_WIN = PLATFORM == "nt"
