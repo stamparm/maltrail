9613fba7c720c7e29c760223ade6ed8e565d3126
Miroslav Stampar <miroslav.stampar@gmail.com>
2021-06-08 14:08:31 +0200

    Tag version bump

--- core/settings.py
+++ core/settings.py
@@ -23,7 +23,7 @@ from core.trailsdict import TrailsDict
 from thirdparty.six.moves import urllib as _urllib
 
 NAME = "Maltrail"
-VERSION = "0.33.1"
+VERSION = "0.34.1"
 HOMEPAGE = "https://maltrail.github.io"
 PLATFORM = os.name
 IS_WIN = PLATFORM == "nt"
