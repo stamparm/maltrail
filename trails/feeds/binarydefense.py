4d9d3da57125792e84b2b6f9b6133b84adc08852
Miroslav Stampar <miroslav.stampar@gmail.com>
2021-07-27 17:39:20 +0200

    Adding new feed

---
+++ trails/feeds/binarydefense.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+"""
+Copyright (c) 2014-2021 Maltrail developers (https://github.com/stamparm/maltrail/)
+See the file 'LICENSE' for copying permission
+"""
+
+from core.common import retrieve_content
+
+__url__ = "https://www.binarydefense.com/banlist.txt"
+__check__ = "ATIF feed"
+__info__ = "known attacker"
+__reference__ = "binarydefense.com"
+
+def fetch():
+    retval = {}
+    content = retrieve_content(__url__)
+
+    if __check__ in content:
+        for line in content.split('\n'):
+            line = line.strip()
+            if not line or line.startswith('#') or '.' not in line:
+                continue
+            retval[line] = (__info__, __reference__)
+
+    return retval
