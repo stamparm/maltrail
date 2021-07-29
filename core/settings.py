580204198308f9f67fff5e6041b544933b0192d5
Miroslav Stampar <miroslav.stampar@gmail.com>
2021-07-29 17:48:45 +0200

    Removing FP

--- core/settings.py
+++ core/settings.py
@@ -23,7 +23,7 @@ from core.trailsdict import TrailsDict
 from thirdparty.six.moves import urllib as _urllib
 
 NAME = "Maltrail"
-VERSION = "0.35.10"
+VERSION = "0.35.11"
 HOMEPAGE = "https://maltrail.github.io"
 PLATFORM = os.name
 IS_WIN = PLATFORM == "nt"
