886da5bde55128390bdab5d0345bdf47f1ebd0f4
Miroslav Stampar <miroslav.stampar@gmail.com>
2023-04-05 10:18:38 +0200

    Adding new feed #19149

---
+++ trails/feeds/blackholemonster.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+"""
+Copyright (c) 2014-2023 Maltrail developers (https://github.com/stamparm/maltrail/)
+See the file 'LICENSE' for copying permission
+"""
+
+from core.common import retrieve_content
+
+__url__ = "https://ip.blackhole.monster/blackhole-today"
+__info__ = "known attacker"
+__reference__ = "ip.blackhole.monster"
+
+def fetch():
+    retval = {}
+    content = retrieve_content(__url__)
+
+    for line in content.split('\n'):
+        line = line.strip()
+        if not line or line.startswith('#') or '.' not in line:
+            continue
+        retval[line] = (__info__, __reference__)
+
+    return retval
