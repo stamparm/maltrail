b59b9cc32f6d9151277afbd9db4a6ae9613d6020
Mikhail Kasimov <mikhail.kasimov@gmail.com>
2023-08-01 08:38:49 +0300

    0.60

--- core/settings.py
+++ core/settings.py
@@ -23,7 +23,7 @@ from core.trailsdict import TrailsDict
 from thirdparty.six.moves import urllib as _urllib
 
 NAME = "Maltrail"
-VERSION = "0.59"
+VERSION = "0.60"
 HOMEPAGE = "https://maltrail.github.io"
 PLATFORM = os.name
 IS_WIN = PLATFORM == "nt"
