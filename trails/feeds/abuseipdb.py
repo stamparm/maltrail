93f99506b5414e2bbe184b2dd83ae334138dcc68
Miroslav Stampar <miroslav.stampar@gmail.com>
2023-10-09 10:56:32 +0200

    Fixes #19208

--- trails/feeds/abuseipdb.py
+++ trails/feeds/abuseipdb.py
@@ -5,12 +5,10 @@ Copyright (c) 2014-2023 Maltrail developers (https://github.com/stamparm/maltrai
 See the file 'LICENSE' for copying permission
 """
 
-import re
-
 from core.common import retrieve_content
 
-__url__ = "https://www.abuseipdb.com/statistics"
-__check__ = "distinct users"
+__url__ = "https://raw.githubusercontent.com/borestad/blocklist-abuseipdb/main/abuseipdb-s100-1d.ipv4"
+__check__ = ".1"
 __info__ = "known attacker"
 __reference__ = "abuseipdb.com"
 
@@ -19,7 +17,10 @@ def fetch():
     content = retrieve_content(__url__)
 
     if __check__ in content:
-        for ip in re.findall(r">(\d+\.\d+\.\d+\.\d+)</a></b> \(\d+ reports from \d+ distinct users\)", content):
-            retval[ip] = (__info__, __reference__)
+        for line in content.split('\n'):
+            line = line.strip()
+            if not line or line.startswith('#') or '.' not in line:
+                continue
+            retval[line] = (__info__, __reference__)
 
     return retval
