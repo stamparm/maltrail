3a8eaa863831300ab3035c4db91f54925f75a43f
Mikhail Kasimov <mikhail.kasimov@gmail.com>
2024-05-09 07:19:25 +0200

    Patch for #19253

--- trails/feeds/cruzit.py
+++ trails/feeds/cruzit.py
@@ -7,7 +7,7 @@ See the file 'LICENSE' for copying permission
 
 from core.common import retrieve_content
 
-__url__ = "https://www.cruzit.com/xxwbl2txt.php"
+__url__ = "https://www.cruzit.com/xxwbl2txt1.php"
 __check__ = "ipaddress"
 __info__ = "known attacker"
 __reference__ = "cruzit.com"
