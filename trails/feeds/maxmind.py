c9c7dee028afdf5d36c0d5038b017cd72f2fba92
Miroslav Stampar <miroslav.stampar@gmail.com>
2025-03-03 19:00:18 +0100

    Fixes MaxMind feed

--- trails/feeds/maxmind.py
+++ trails/feeds/maxmind.py
@@ -9,8 +9,8 @@ import re
 
 from core.common import retrieve_content
 
-__url__ = "https://www.maxmind.com/en/high-risk-ip-sample-list"
-__check__ = "Sample List of Higher Risk IP Addresses"
+__url__ = "https://iplists.firehol.org/files/maxmind_proxy_fraud.ipset"
+__check__ = "[MaxMind.com]"
 __info__ = "bad reputation (suspicious)"
 __reference__ = "maxmind.com"
 
@@ -18,8 +18,10 @@ def fetch():
     retval = {}
     content = retrieve_content(__url__)
 
-    if __check__ in content:
-        for match in re.finditer(r"high-risk-ip-sample/([\d.]+)", content):
-            retval[match.group(1)] = (__info__, __reference__)
+    for line in content.split('\n'):
+        line = line.strip()
+        if not line or line.startswith('#') or '.' not in line:
+            continue
+        retval[line] = (__info__, __reference__)
 
     return retval
