43365c0b3d110e6643cba21e993d0a36d72f79a7
chars6 <95272438+chars6@users.noreply.github.com>
2025-03-22 00:46:06 +0800

    Update blackholemonster.py
    having problems with database (appending error messages to the end of gzip stream)

--- trails/feeds/blackholemonster.py
+++ trails/feeds/blackholemonster.py
@@ -6,14 +6,14 @@ See the file 'LICENSE' for copying permission
 """
 
 from core.common import retrieve_content
-
+from core.settings import NAME
 __url__ = "https://blackhole.s-e-r-v-e-r.pw/blackhole-today"
 __info__ = "known attacker"
 __reference__ = "ip.blackhole.monster"
 
 def fetch():
     retval = {}
-    content = retrieve_content(__url__)
+    content = retrieve_content(__url__, headers={"User-agent": NAME}) # having problems with database (appending error messages to the end of gzip stream)
 
     for line in content.split('\n'):
         line = line.strip()
