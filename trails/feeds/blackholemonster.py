747350c0e18de4a15204df5305d0072c07130485
Miroslav Stampar <miroslav.stampar@gmail.com>
2025-06-06 21:14:24 +0200

    Fix related to #19317

--- trails/feeds/blackholemonster.py
+++ trails/feeds/blackholemonster.py
@@ -8,6 +8,7 @@ See the file 'LICENSE' for copying permission
 from core.common import retrieve_content
 
 __url__ = "https://blackhole.s-e-r-v-e-r.pw/blackhole-today"
+__check__ = ".1"
 __info__ = "known attacker"
 __reference__ = "ip.blackhole.monster"
 
@@ -15,10 +16,11 @@ def fetch():
     retval = {}
     content = retrieve_content(__url__)
 
-    for line in content.split('\n'):
-        line = line.strip()
-        if not line or line.startswith('#') or '.' not in line:
-            continue
-        retval[line] = (__info__, __reference__)
+    if __check__ in content:
+        for line in content.split('\n'):
+            line = line.strip()
+            if not line or line.startswith('#') or '.' not in line:
+                continue
+            retval[line] = (__info__, __reference__)
 
     return retval
