2e387a7ca6f0888b9d7e9692e2564d9fcb75ba2e
Miroslav Stampar <miroslav.stampar@gmail.com>
2026-06-12 17:49:30 +0200

    Couple of patches

--- feeds/maxmind.py
+++ feeds/maxmind.py
@@ -18,10 +18,11 @@ def fetch():
     retval = {}
     content = retrieve_content(__url__)
 
-    for line in content.split('\n'):
-        line = line.strip()
-        if not line or line.startswith('#') or '.' not in line:
-            continue
-        retval[line] = (__info__, __reference__)
+    if __check__ in content:
+        for line in content.split('\n'):
+            line = line.strip()
+            if not line or line.startswith('#') or '.' not in line:
+                continue
+            retval[line] = (__info__, __reference__)
 
     return retval
