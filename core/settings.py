4386ddad32262f922e95bc75242b89c001cb77fa
Miroslav Stampar <miroslav.stampar@gmail.com>
2026-08-07 11:00:58 +0200

    Changing version to 3.0

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ from core.trailsdict import TrailsDict
 from thirdparty.six.moves import urllib as _urllib
 
 NAME = "Maltrail"
-VERSION = "2.2"
+VERSION = "3.0"
 HOMEPAGE = "https://maltrail.github.io"
 PLATFORM = os.name
 IS_WIN = PLATFORM == "nt"
